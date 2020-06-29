a1cb29f111b17f1ca5954ac8ebb7b462a679dcc0
AmjadMoqade98 <Amjad.Moqade@exalt.email>
2020-06-29 15:13:47 +0300

    no message

--- src/main/java/com/Training/BackEnd/Constants.java
+++ src/main/java/com/Training/BackEnd/Constants.java
@@ -3,7 +3,7 @@ package com.Training.BackEnd;
 public final class Constants {
 
     public static final String  SoapNamespace = "http://amjad.com/bundles" ;
-    public static final String SoapUri = "http://172.17.0.5:8098/SoapBundles" ;
+    public static final String SoapUri = "http://172.18.0.2:8098/SoapBundles" ;
     public static final String MarshallerPackage = "com.Training.BackEnd.wsdl" ;
     public static final String SoapFile = "bundles.xsd" ;
 }
